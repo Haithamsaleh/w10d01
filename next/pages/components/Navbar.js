5cec5a338ceec0328947fb862689e45fcfd7d38b
Haithamsaleh <Haitham.saleh.elq.js@tuwaig.edu.sa>
2021-12-12 14:48:54 +0300

    logo

--- next/pages/components/Navbar.js
+++ next/pages/components/Navbar.js
@@ -1,8 +1,10 @@
 import Link from "next/link";
-
+import Image from "next/image";
 const NavBar = () => {
-    return (   <div>
+    return (   
+    <div>
         <ul>
+            <Link href="/"><li><Image src="/logo.png" width={100} height={100} /></li></Link>
             <Link href="/" ><li>Home</li></Link>
             <Link href="/about"><li>About</li></Link>
             <Link href="/contact"><li>contact</li></Link>
